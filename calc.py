71bf952ab361261fdcd7bfefcc2202ad12f3c20d
Colin Cron <colint.cron@gmail.com>
2020-09-30 22:05:17 -0400

    first commit

---
+++ calc.py
@@ -0,0 +1,63 @@
+
+def print_space():
+    print(" ")
+
+def print_menu():
+    print("*" * 20)
+    print_space()
+    print("  Python Calculator")
+    print_space()
+    print("*" * 20)
+
+    print_space()
+    print("[1] Add")
+    print("[2] Subtract")
+    print("[3] Multiply")
+    print("[4] Divide")
+    print("[5] My age")
+    print_space()
+    print_space()
+    print("[0] Exit")
+    print_space()
+
+
+#instruction
+
+opc = ''
+while( opc != '0' ):
+
+    print_menu()
+
+    opc = input('Please Choose An Option: ')
+
+
+    if(opc != '0' & opc !='5'):
+        num1 = input("First number: ")
+        num2 = input("Second number: ")
+
+    if(opc == '1'):
+        res = float(num1) + float(num2)
+        print("Result: " + str(res))
+
+    elif(opc == '2'):
+        res = float(num1) - float(num2)
+        print("Result: " + str(res))
+
+    elif(opc == '3'):
+        res = float(num1) * float(num2)
+        print("Result: " + str(res))
+
+    elif(opc == '4'):
+        if(float(num2) != 0):
+            res = float(num1) / float(num2)
+            print("Result: " + str(res))
+        else:
+            print("WARNING -- YOU WILL DESTROY THE UNIVERSE")
+    
+    elif(opc == '5')
+        year = input("What year were you born? ")
+        age = 2020 - int(year)
+        print("You are " + age + " years old.")
+        
+
+print("Goodbye...")
