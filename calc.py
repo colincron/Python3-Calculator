7fd4ce24b742bc7799c81c93e8e79cc66051e3c8
Colin Cron <colint.cron@gmail.com>
2020-10-01 14:51:55 -0400

    Very colorful

--- calc.py
+++ calc.py
@@ -3,36 +3,42 @@ def print_space():
     print(" ")
 
 def print_menu():
-    print("*" * 21)
+    print(cyan_text + "*" * 21 + color_default)
     print_space()
-    print("  Python Calculator")
+    print(cyan_text + "  Python Calculator" + color_default)
     print_space()
-    print("*" * 21)
+    print(cyan_text + "*" * 21 + color_default)
 
     print_space()
-    print("[1] Add")
-    print("[2] Subtract")
-    print("[3] Multiply")
-    print("[4] Divide")
-    print("[5] Exponents")
+    print(blue_text + "[1] Add" + color_default)
+    print(yellow_text + "[2] Subtract" + color_default)
+    print(blue_text + "[3] Multiply" + color_default)
+    print(yellow_text + "[4] Divide" + color_default)
+    print(blue_text + "[5] Exponents" + color_default)
     print_space()
-    print("[8] Even or Odd?")
+    print(red_text + "[8] Even or Odd?")
     print_space()
-    print("[9] My age")
+    print("[9] My age" + color_default)
     print_space()
     print_space()
-    print("[0] Exit")
+    print(red_underline + "[0] Exit" + color_default)
     print_space()
 
 def press_enter():
-    input("Press Enter to continue...")
+    input(yellow_text + "Press Enter to continue...\n\n\n" + color_default)
 
 
 #instruction
 
-green_text = "\033[32m"
+blue_text = "\033[34;1m"
+yellow_text = "\033[33m"
+green_text = "\033[32;1m"
 red_text = "\033[31;1m"
+cyan_text = "\033[36;1m"
+red_underline = "\033[31;5m"
+
 color_default = "\033[m"
+
 
 opc = ''
 while( opc != '0' ):
@@ -66,7 +72,7 @@ while( opc != '0' ):
             print(red_text + "Result: " + str(res)  + color_default +  "\n")
             press_enter()
         else:
-            print(red_text + "WARNING -- YOU WILL DESTROY THE UNIVERSE!" + color_default +  "\n")
+            print(red_text + "\n\n\nWARNING -- YOU WILL DESTROY THE UNIVERSE!" + color_default +  "\n")
             press_enter()
     
     elif(opc == '5'):
